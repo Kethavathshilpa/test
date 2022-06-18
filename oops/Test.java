824c5f1909d2a91ca4c215579fd14de0d260247d
Kethavathshilpa <kethavathShilpa1999@gmail.com>
2022-06-18 15:54:07 +0530

    oops

---
+++ oops/Test.java
@@ -0,0 +1,16 @@
+class Test 
+{
+	void show() 
+	{
+		System.out.println("1");
+	}
+	void show()
+	{
+		System.out.println(int a);
+	}
+	public static void main(String args[])
+	{
+		Test t=new Test();
+		t.show(10);
+	}
+}
